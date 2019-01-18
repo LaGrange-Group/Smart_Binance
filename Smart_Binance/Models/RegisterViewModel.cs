30294aa8f41cbcbb9d2a2301f06da4f00d0ec9cb
Lagrango <davidnlagrange@hotmail.com>
2019-01-18 10:27:21 -0600

    Revamped Registration Page

---
+++ Smart_Binance/Models/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Smart_Binance.Areas.Identity.Pages.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smart_Binance.Models
+{
+    public class RegisterViewModel
+    {
+        public RegisterModel  RegisterModel { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
