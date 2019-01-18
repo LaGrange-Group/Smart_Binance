9a5776ad574701d582af48dc8932ce0bf37cdd1a
Lagrango <davidnlagrange@hotmail.com>
2019-01-18 08:07:12 -0600

    Completed Roles for Users

---
+++ Smart_Binance/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Smart_Binance.Data;
+
+[assembly: HostingStartup(typeof(Smart_Binance.Areas.Identity.IdentityHostingStartup))]
+namespace Smart_Binance.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Smart_Binance/StaticDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smart_Binance
+{
+    public class StaticDetails
+    {
+        public const string CustomerEndUser = "Customer";
+    }
+}
