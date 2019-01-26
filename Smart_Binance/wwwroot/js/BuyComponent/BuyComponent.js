6f64f44950a368f8ed5029b6abbedb935bb77805
Lagrango <davidnlagrange@hotmail.com>
2019-01-26 14:23:40 -0600

    Update Components on Base Currency Percent Button Group

--- Smart_Binance/wwwroot/js/BuyComponent/BuyComponent.js
+++ Smart_Binance/wwwroot/js/BuyComponent/BuyComponent.js
@@ -4,5 +4,14 @@
 
 $(".percTypeBtn").click(function () {
     $(".percTypeBtn").removeClass("active");
+    var percent = $(this).val();
+    var baseAmountCalc = baseTotal * percent;
+    $('#basetotal').val(baseAmountCalc);
+    alert(countDecimals(amount));
+    $('#amount').val(baseAmountCalc / lastPrice).toFixed(amount.countDecimals());
 });
 
+var countDecimals = function (value) {
+    if (Math.floor(value) === value) return 0;
+    return value.toString().split(".")[1].length || 0;
+}
